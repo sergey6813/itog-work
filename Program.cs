fe40571965f5a10353141ad77862ad445603a12f
sergey <serezha.divak@gmail.com>
2022-01-31 14:10:02 +0300

    добавили файл с кодом

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+
+{
+static void Resize(ref int [] array, int newSize)
+{
+int[] newArray = new int[newSize];
+
+for (int i = 0; i < array.Length && i < newArray.Length;i++)
+    newArray[i] = array[i];
+
+    array = newArray;
+    
+}
+static void Main(string[]args)
+{
+int[] myArray = {1,3,4,5,6,6};
+   
+     Resize(ref myArray, 3);
+
+} 
+
+}
+
+
